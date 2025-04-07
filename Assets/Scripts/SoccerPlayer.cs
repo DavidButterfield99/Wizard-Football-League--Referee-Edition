78fb74a3aac51810994fc60a753b736d87f3db18
Tyler Jordan <1997.tylerjordan@gmail.com>
2025-04-07 21:15:05 +0300

    Players will now chase the ball

--- Assets/Scripts/SoccerPlayer.cs
+++ Assets/Scripts/SoccerPlayer.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class SoccerPlayer : MonoBehaviour 
 {
     public GameObject ball;
-    float spellCasting;
-    float healthPoints;
-    float strength;
-    float speedMax;
-    float acceleration;
-    string team;
-    string role;
+    public float spellCasting;
+    public float healthPoints = 50f;
+    public float strength = 50f;
+    public float speedMax = 50f;
+    public float acceleration = 50f;
+    public string team;
+    public string role;
 
     public float GetDistanceFromBall() {
         float distance = Vector3.Distance(transform.position, ball.transform.position);
--- Assets/Scripts/TeamController.cs
+++ Assets/Scripts/TeamController.cs
@@ -1,10 +1,12 @@
+using UnityEditor.AnimatedValues;
 using UnityEngine;
 
-public class TeamController : MonoBehaviour 
+public class TeamController : MonoBehaviour
 {
+    GameObject ball;
     public SoccerPlayer soccerPlayerPrefab;
-    SoccerPlayer[] players;
-    public Vector3[] startingPositions = new Vector3[6]; 
+    SoccerPlayer[] players = new SoccerPlayer[6];
+    public Vector3[] startingPositions = new Vector3[6];
     /*
     Player order goes:
     0: Goalkeeper
@@ -13,18 +15,29 @@ public class TeamController : MonoBehaviour
     */
 
 
-    private void Start() {
-        SpawnPlayers();        
+    private void Start()
+    {
+        ball = GameObject.Find("Ball");
+        SpawnPlayers();
     }
-    
+
+    private void Update()
+    {
+        foreach (var player in players)
+        {
+            player.transform.LookAt(ball.transform);
+            player.transform.Translate(Vector3.forward * player.speedMax * Time.deltaTime);
+        }
+    }
+
+
     void SpawnPlayers()
     {
         for (int i = 0; i < 6; i++)
         {
-            Instantiate(soccerPlayerPrefab, startingPositions[i], soccerPlayerPrefab.transform.rotation);
+            SoccerPlayer player = Instantiate(soccerPlayerPrefab, startingPositions[i], soccerPlayerPrefab.transform.rotation);
+            players[i] = player;
         }
-
-
     }
-    
+
 }
