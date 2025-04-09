4635452e7845d6cbf86452ed2eceff62d073ab8f
DavidButterfield99 <89974746+DavidButterfield99@users.noreply.github.com>
2025-04-09 18:25:00 +0300

    ?

--- Assets/Scripts/MenuController1.cs
+++ Assets/Scripts/MenuController1.cs
@@ -4,22 +4,35 @@ public class MenuController1 : MonoBehaviour
 {
     public GameObject creditsPanel;
     private Animator creditsPanelAnimator;
+    public GameObject settingsPanel;
+    private Animator settingsPanelAnimator;
 
     void Start()
     {
-        {
-            if (creditsPanel != null) {
-                creditsPanelAnimator = creditsPanel.GetComponent<Animator>();
-                creditsPanelAnimator.SetBool("_isOpen", false);
-            }
+        if (creditsPanel != null) {
+            creditsPanelAnimator = creditsPanel.GetComponent<Animator>();
+            creditsPanelAnimator.SetBool("_isOpen", false);
+        }
+        if (settingsPanel != null) {
+            settingsPanelAnimator = settingsPanel.GetComponent<Animator>();
+            settingsPanelAnimator.SetBool("_isOpen", false);
         }
     }
+
 
     public void OpenCredits() {
         if (creditsPanelAnimator.GetBool("_isOpen")) {
             creditsPanelAnimator.SetBool("_isOpen", false);
         } else {
             creditsPanelAnimator.SetBool("_isOpen", true);
+        }
+    }
+
+    public void OpenSettings() {
+        if (settingsPanelAnimator.GetBool("_isOpen")) {
+            settingsPanelAnimator.SetBool("_isOpen", false);
+        } else {
+            settingsPanelAnimator.SetBool("_isOpen", true);
         }
     }
 }
