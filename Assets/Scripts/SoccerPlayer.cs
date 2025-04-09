f0e893ed213c4d51cfe7d3e384501ebedb26ee71
Tyler Jordan <1997.tylerjordan@gmail.com>
2025-04-09 13:55:06 +0300

    Added defender logic

--- Assets/Scripts/SoccerPlayer.cs
+++ Assets/Scripts/SoccerPlayer.cs
@@ -4,17 +4,23 @@ public class SoccerPlayer : MonoBehaviour
 {
     GameObject ball;
     Rigidbody ballRb;
+    Rigidbody playerRb;
     public float spellCasting;
     public float healthPoints = 50f;
     public float strength = 50f;
-    public float speedMax = 50f;
+    public float speedMax = 350f;
     public float acceleration = 50f;
     public string team;
     public string role;
+    public int defenceZone; // 1 or 2
+
 
     private float avoidRadius = 15f;
     private bool isBallBlocking;
     private int forceDivider = 5;
+    private float boundryX = 0;
+
+    private Vector3 moveDirection;
 
     public GameObject goalObject;
 
@@ -23,6 +29,8 @@ public class SoccerPlayer : MonoBehaviour
     {
         ball = GameObject.Find("Ball");
         ballRb = ball.GetComponent<Rigidbody>();
+        playerRb = GetComponent<Rigidbody>();
+
     }
 
 
@@ -42,33 +50,54 @@ public class SoccerPlayer : MonoBehaviour
 
     public void RunTowardsBall()
     {
+        if (transform.position.y < 20)
+        {
+            Vector3 newPos = new Vector3(transform.position.x, 25, transform.position.y);
+            transform.position = newPos;
+        }
+
         float ballToGoal = Vector3.Distance(ball.transform.position, goalObject.transform.position);
         float playerToGoal = Vector3.Distance(transform.position, goalObject.transform.position);
-        
+
 
         isBallBlocking = ballToGoal + avoidRadius > playerToGoal;
 
         Vector3 behindBall = ball.transform.position - goalObject.transform.position * avoidRadius;
-        Vector3 moveDirection = ball.transform.position;
+        moveDirection = ball.transform.position;
 
-        Debug.Log(isBallBlocking);
         if (isBallBlocking)
         {
             moveDirection = behindBall + (Vector3.right * speedMax * Time.deltaTime);
         }
 
+        if (role == "Defender")
+        {
+            if (ball.transform.position.x > boundryX && team == "1" || ball.transform.position.x < boundryX && team == "2")
+            {
+                moveDirection.x = 0;
+            }
 
+            if (ball.transform.position.z < 0 && defenceZone == 1)
+            {
+                moveDirection.z = 20;
+            }
+            if (ball.transform.position.z > 0 && defenceZone == 2)
+            {
+                moveDirection.z = -20;
+            }
+        }
 
+        moveDirection.y = 20;
         transform.LookAt(moveDirection);
         transform.Translate(Vector3.forward * speedMax * Time.deltaTime);
     }
 
     public void KickBall()
     {
-        Debug.Log("Kicking ball!");
         Vector3 kickDirection = ball.transform.position - transform.position;
         if (!isBallBlocking)
         {
+            Debug.Log("Kicking ball!");
             ballRb.AddForce(kickDirection * strength / forceDivider, ForceMode.Impulse);
         }
     }
